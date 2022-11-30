56ae42428049fb30b352769c8a6bec5c55b4cfcf
anyaachan <anna.danchenko@icloud.com>
2022-11-30 23:01:26 +0100

    Improvements

--- ICA2/app-with-graph.py
+++ ICA2/app-with-graph.py
@@ -1,4 +1,6 @@
 from dash import Dash, dcc, Output, Input, html
+import numpy as np
+import json
 import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.express as px  # Library that allows you to build graphs
@@ -11,6 +13,33 @@ df = pd.read_csv("ICA2/data-cleaned.csv")
 total_cases = df["New cases"].sum()
 total_deaths = df["New deaths"].sum()
 
+# "Converting" to a logarithm scale
+df['cases_color'] = df['New cases'].apply(np.log10)
+max_log = int(df['cases_color'].max())
+values = [i for i in range(max_log + 1)] #list comprehension
+ticks = [10**i for i in values]
+
+# Preparing a map for chloropleth graph
+# Loading json file
+world_path = "ICA2/custom.geo.json"
+with open(world_path) as f:
+    geo_world = json.load(f)
+
+countries_geo = []
+
+# Looping 
+for country in geo_world['features']:
+
+    country_name = country['properties']['name'] 
+    geometry = country['geometry']
+
+    countries_geo.append({
+            'type': 'Feature',
+            'geometry': geometry,
+            'id':country_name
+        })
+geo_world_ok = {'type': 'FeatureCollection', 'features': countries_geo}
+
 # Building our components
 # Selecting a theme and opening the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
@@ -20,9 +49,31 @@ my_title = dcc.Markdown(
 # Making graphs components
 # The figure is empty, there is nothing at the beginning
 my_graph = dcc.Graph(figure={})
+
 my_bar = dcc.Graph(figure=px.histogram(df, x = "Month", y = "New cases",
                    color = "continent", title = "Cases distribution by continent for each month"))
 
+chor = px.choropleth_mapbox(
+        df, geojson = geo_world_ok, 
+        locations = "Country", 
+        color='New cases',
+        color_continuous_scale="Viridis",
+        animation_frame = "Month",
+        mapbox_style='open-street-map',
+        zoom=0.5,
+        center={'lat': 19, 'lon': 11},
+        opacity=0.6
+    )
+
+chor.update_layout(
+    coloraxis_colorbar={
+        'title':'Confirmed people',
+        'tickvals':values,
+        'ticktext':ticks        
+    }
+)
+
+my_chor = dcc.Graph(figure = chor)
 # Making selection components
 dropdown_cases = dcc.Dropdown(options=["New cases", "New deaths"],
                               value="New cases",  # Setting the initial value
@@ -73,7 +124,7 @@ app.layout = html.Div(children=[
 
     html.Div(children=[
         html.Div(children=[
-            my_bar
+            my_chor
         ]),
     ], style={"border": "1px solid", "border-radius": "13px", "padding": "30px"}),
 
@@ -92,9 +143,10 @@ def update_graph(cases_input, continents_input):
     # df_filtered = df[df["year"] == int(year_input)]
     mask = df.continent.isin(continents_input)
 
-    fig = px.line(df[mask], x="date", y=cases_input,
-                  color="Country", title=cases_input + " by day")
-
+    fig = px.line(df[mask], x="date", 
+                y=cases_input,
+                color="Country", title=cases_input + " by day"
+    )
     return fig  # Fig goes into the output -> my_graph
 
 

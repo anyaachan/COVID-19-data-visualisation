280e8dbab6e92988862854f62cd0c9c5e2882284
anyaachan <anna.danchenko@icloud.com>
2022-12-02 20:50:29 +0100

    Commit

--- ICA2/clean_data_new.py
+++ ICA2/clean_data_new.py
@@ -1,28 +1,26 @@
 import pandas as pd
-import numpy as np
 
-df = pd.read_csv("ICA2/covid-data.csv")  # Creating a dataframe
+df = pd.read_csv("ICA2/resources/covid-data.csv")  # Creating a dataframe
 
-#Defining variable in which we store columns that we don't need.
-
-# Deleting selected columns. We write inplace=True to make changes directly to the object instead of a copy.
-
+# Renaming columns to more convinient names
 df.rename({"new_cases": "New cases", "new_deaths": "New deaths",
           "location": "Country"}, axis=1, inplace=True)
 
-
+# Deleting rows with null values from colum])
 df = df.dropna(axis=0, subset=['continent'])
 
+# Converting column "date" into date/time format.
 df["date"] = pd.to_datetime(df["date"])
 
-
+# Creating new column in which we store month of the year.
 df['Month'] = pd.to_datetime(df['date']).dt.to_period('M')
 
 # Optimizing dataset by leaving only necessary columns
-to_leave = ["Country", "New cases", "New deaths", "date", "Month", "continents"]
-df = df[to_leave]
+# Defining variable in which we store columns that we don't need.
+to_leave = ["Country", "New cases", "New deaths", "date", "Month", "continent"]
+df = df[to_leave]  # Leaving only selected columns.
 
 print(df.head(10))  # We look at the first five entries using "head" method
 print(df.info())
 
-df.to_csv("data-cleaned.csv")
+df.to_csv("data-cleaned.csv")  # Exporting cleaned data into the new file
